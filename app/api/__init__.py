654959057de5d2e4b033db4ea293a90c182d0a58
Abraham Mtz. <carlosmontes0983@gmail.com>
2026-07-12 00:15:25 -0600

    refactor: aggregate API routers

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,11 @@
-from app.api.routes import router
+from fastapi import APIRouter
+
+from app.api.routes import router as control_router
+from app.api.volume_routes import router as volume_router
+
+
+router = APIRouter()
+router.include_router(control_router)
+router.include_router(volume_router)
 
 __all__ = ["router"]
